55f642855a51b6100e2fd09a73399ea748fe1f6c
marciodsantos <marcioluisborgesds@gmail.com>
2022-03-08 19:53:15 -0300

    Localhost

--- src/Main.js
+++ src/Main.js
@@ -4,18 +4,17 @@ import Home from './components/home/home';
 import About from './components/about/about';
 import Contact from './components/contact/contact';
 import Forms from './components/forms/forms';
+import {BrowserRouter, Switch, Route, Link} from 'react-router-dom'
 
 
-function Main() {
+export default function Main() {
   return (
     <>
-      <Home />
-      <About />
-      <Contact />
-      <Forms />
+        <Home />
+        <About />
+        <Contact />
+        <Forms />
     </>
   );
 }
 
-
-export default Main;
