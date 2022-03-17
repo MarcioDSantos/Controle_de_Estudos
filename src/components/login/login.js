8589913ee49989a8fd69608da83e1df2c6cf3677
Murillo José de Azevedo <murilloazevedo2@gmail.com>
2022-03-16 21:51:37 -0300

    modificação do layput
    Consegui modificar um pouco do layout anterior, fazendo uma divisão entre área com texto de boas vindas, e outra para realizar o login

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -11,39 +11,45 @@ function Login(){
             <div className="container-login">
                 <div className="wrap-login">
                     <form className="login-form">
-                        <span className="login-form-title">
-                            Bem Vindo!
-                        </span>
-                        <span className="login-form-title">
-                            <img src={Prog} alt="Programador"></img>
-                        </span>
-
-                        <div className='wrap_input'>
-                           <input className={email !== "" ? 'has-val input' : 'input'} 
-                                type="email" 
-                                value={email}
-                                onChange={e => setEmail(e.target.value)}
-                           />
-                           <span className='focus-input' data-placeholder='E-mail'></span>
+                        <div className="firstContent"> 
+                            <span className="login-form-title">
+                                Bem Vindo Dev!
+                            </span>
+                            <span className="login-form-title">
+                                <img src={Prog} alt="Programador"></img>
+                            </span>
                         </div>
 
-                        <div className='wrap_input'>
-                           <input className={password !== "" ? 'has-val input' : 'input'}  
-                                type="password"
-                                value={password}
-                                onChange={e => setPassword(e.target.value)}
-                           />
-                           <span className='focus-input' data-placeholder='PassWord'></span>
-                        </div>
-                            
-                        <div className='container-login-form-btn'>
-                            <button className='login-form-btn'>Login</button>
-                        </div>
+                        <div className="secondContent">    
+                            <div className='wrap_input'>
+                            <input className={email !== "" ? 'has-val input' : 'input'} 
+                                    type="email" 
+                                    value={email}
+                                    onChange={e => setEmail(e.target.value)}
+                            />
+                            <span className='focus-input' data-placeholder='E-mail'></span>
+                            </div>
+                        
 
-                        <div className='text-center'>
-                            <span className='txt1'>Esqueceu a sua senha?</span>
+                            <div className='wrap_input'>
+                            <input className={password !== "" ? 'has-val input' : 'input'}  
+                                    type="password"
+                                    value={password}
+                                    onChange={e => setPassword(e.target.value)}
+                            />
+                            <span className='focus-input' data-placeholder='PassWord'></span>
+                            </div>
+                                
+                            <div className='container-login-form-btn'>
+                                <button className='login-form-btn'>Login</button>
+                            </div>
+                        
 
-                            <a className='txt2' href='#'>Recurperar senha</a>
+                            <div className='text-center'>
+                                <span className='txt1'>Esqueceu a sua senha?</span>
+
+                                <a className='txt2' href='#'>Recurperar senha</a>
+                            </div>
                         </div>
 
                     </form>
